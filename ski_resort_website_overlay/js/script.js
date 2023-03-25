1f640ac0406bdcacd2e90c8deef056aacc9733f6
perezzz1 <118335947+perezzz1@users.noreply.github.com>
2023-03-25 23:24:27 +0400

    Add files via upload

---
+++ ski_resort_website_overlay/js/script.js
@@ -0,0 +1,27 @@
+const searchBth = document.querySelector('.search-btn')
+const searchInput = document.querySelector('.search-input')
+const searchBox= document.querySelector('.search-box')
+const main = document.querySelector('main')
+
+searchBth.addEventListener('click', function (e) {
+    e.stopPropagation();
+    searchBox.classList.add('active');
+    searchInput.classList.add('active');
+});
+
+
+document.addEventListener('click', function (e) {
+    if (e.target !== searchBox) {
+        searchBox.classList.remove('active');
+        searchInput.classList.remove('active');
+    }
+});
+
+// main.addEventListener('click', function (e) {
+//     e.stopPropagation();
+//     searchBox.classList.remove('active');
+//     searchInput.classList.remove('active');
+// });
+
+
+
